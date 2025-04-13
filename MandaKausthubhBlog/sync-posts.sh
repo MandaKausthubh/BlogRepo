760a3f72f67bdc3cbe6210f2064624261367e2e7
Manda Kausthubh <manda.kausthubh@gmail.com>
2025-04-13 15:21:37 +0530

    Created an auto .sh command

---
+++ MandaKausthubhBlog/sync-posts.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rsync -av \
+  --include="*.md" \
+  --include="*/" \
+  --include="*.png" \
+  --include="*.jpg" \
+  --include="*.pdf" \
+  --exclude="*" \
+  ~/Documents/ResearchVault/posts/ Ghost/Blog/MandaKausthubhBlog/content/posts/
+cd Ghost/Blog/MandaKausthubhBlog/
+cd public
+git add .
+git commit -m "Update notes $(date)"
+git push
